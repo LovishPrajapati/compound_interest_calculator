7e6fcbff4ecce354e613d02ed3ccb1af74c4365d
LovishPrajapati <prajapatilovish@gmail.com>
2020-11-15 21:18:52 +0530

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <h4>Made by Lovish Kumar Prajapati</h4>
+    </div>
+  );
+}
+
+export default Footer;
